d431c2d46b30ecc6f823e81b37be43f4efcba819
JoelC91 <joel.carrizo.91@gmail.com>
2019-06-21 19:53:14 -0300

    initial commit

---
+++ principal.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace login_WFA
+{
+    partial class principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(122, 70);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(100, 20);
+            this.txt_user.TabIndex = 0;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(122, 103);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(100, 20);
+            this.txt_password.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(12, 176);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.Btn_login_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(147, 176);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.Btn_exit_Click);
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Location = new System.Drawing.Point(58, 70);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(29, 13);
+            this.lbl_user.TabIndex = 4;
+            this.lbl_user.Text = "User";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(34, 103);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(53, 13);
+            this.lbl_password.TabIndex = 5;
+            this.lbl_password.Text = "Password";
+            // 
+            // lbl_login
+            // 
+            this.lbl_login.AutoSize = true;
+            this.lbl_login.Location = new System.Drawing.Point(54, 9);
+            this.lbl_login.Name = "lbl_login";
+            this.lbl_login.Size = new System.Drawing.Size(33, 13);
+            this.lbl_login.TabIndex = 6;
+            this.lbl_login.Text = "Login";
+            // 
+            // principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 211);
+            this.Controls.Add(this.lbl_login);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_user);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_user);
+            this.MaximizeBox = false;
+            this.Name = "principal";
+            this.Text = "Programa de login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Label lbl_login;
+    }
+}
+
---
+++ principal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_WFA
+{
+    public partial class principal : Form
+    {
+        public principal()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void Btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_login_Click(object sender, EventArgs e)
+        {
+            if (txt_user.Text == "usuario" && txt_password.Text == "usuario")
+            {
+                MessageBox.Show("Se inicio sesion");
+                txt_user.Text = "";
+                txt_password.Text = "";
+                txt_user.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Eror en el usuario o la contraseña");
+                txt_user.Text = "";
+                txt_password.Text = "";
+                txt_user.Focus();
+            }
+        }
+    }
+}
